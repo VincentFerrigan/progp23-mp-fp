7f3c25167930206ab6943cc206071c4a612f66d2
VincentFerrigan <vincent.ferrigan@gmail.com>
2023-02-28 11:36:36 +0100

    Adding the skeleton code for the 4 tasks

---
+++ src/list_of_numbers.hs
@@ -0,0 +1,16 @@
+{- |
+File name      : list_of_numbers.hs 
+Module         : main
+Description    : Task 1: En lista med heltal
+                 <https://canvas.kth.se/courses/38058/assignments/237975>
+Course         : DD1360 VT23 Programmeringsparadigm (progp23)
+Author/Student : Vincent Ferrigan
+maintainer     : ferrigan@kth.se
+-}
+
+module Main
+    ( -- * Functions
+      -- squarePositive
+    ) where
+
+-- squarePositive :: [Int] -> [Int]
